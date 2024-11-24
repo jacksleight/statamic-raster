bdbcc0025bb7d87920e5c021145184f0a3e98bc1
Jack Sleight <jacksleight@gmail.com>
2024-11-24 12:49:32 +0000

    WIP

--- resources/js/components/fieldtypes/Raster.vue
+++ resources/js/components/fieldtypes/Raster.vue
@@ -24,25 +24,43 @@ export default {
             window.addEventListener('resize', this.measureElement);
             window.addEventListener('message', this.receiveMessage);
         });
-        Statamic.$hooks.on('entry.saved', () => {
-            this.$refs.preview.contentWindow.location.reload();
-        });
+        // Statamic.$hooks.on('entry.saved', () => {
+        //     this.$refs.preview.contentWindow.location.reload();
+        // });
+        setTimeout(() => {
+            this.livePreview = document.getElementById('live-preview-iframe');
+            if (this.livePreview) {
+                this.livePreview.contentWindow.addEventListener('message', (event) => {
+                    console.log(event);
+                    this.$refs.preview.contentWindow.postMessage(event.data);
+                });        
+            }
+        }, 500);
     },
 
     data() {
-
         return {
             previewWidth: null,
             previewRatio: null,
-        }
-
+            livePreview: null,
+            livePreviewParams: null,
+        };
     },
 
     
     computed: {
 
         previewSrc() {
-            return `/!/raster/statamic.hero?content=08f054d5-4254-4434-b89b-44d292c38a0c&preview=1&width=${this.previewWidth}`;
+            let url = '/!/raster/statamic.hero?content=08f054d5-4254-4434-b89b-44d292c38a0c&preview=1';
+            if (this.previewWidth) {
+                url += `&width=${this.previewWidth}`;
+            }
+            if (this.livePreviewParams) {
+                url += `&token=${this.livePreviewParams.token}`;
+                url += `&live-preview=${this.livePreviewParams.value}`;
+            }
+
+            return url;
         },
 
         previewStyle() {
@@ -70,7 +88,7 @@ export default {
             this.previewRatio = data.rect.width / data.rect.height;
         },
 
-    }
+    },
 
 }
 </script>
